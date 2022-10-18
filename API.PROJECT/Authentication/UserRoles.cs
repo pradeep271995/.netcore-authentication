a90b49d19fb3571463c70f5459fd9de6e192c70f
pradeep_kumar <Pradeep_Kumar3@epam.com>
2022-10-18 18:48:53 +0530

    first-commit
    first-commit

---
+++ API.PROJECT/Authentication/UserRoles.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace API.PROJECT.Authentication
+{
+    public class UserRoles
+    {
+        public const string Admin = "Admin";
+        public const string User = "User";
+    }
+}
